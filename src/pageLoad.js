6ab485fc5bc2b45f2784bed999cb3564e897783b
Denis Lafontant <icebox827@gmail.com>
2021-03-24 14:32:57 -0400

    Modify pafeLoader

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -6,8 +6,8 @@ function header() {
   name.classList.add("name");
   name.innnerHTML = "<b>Ô Comptoir</b>";
 
-  header.appendChild("name");
-  header.appendChild("navbar");
+  header.appendChild(name);
+  header.appendChild(navbar());
 
   return header;
 }
@@ -48,4 +48,58 @@ function navbar() {
 
   return nav;
 }
+
+function activeButton(button) {
+  const buttons = document.querySelectorAll(".button-nav");
+
+  buttons.forEach((button) => {
+    if (button !== this) {
+      button.classList.remove("active");
+    }
+  });
+
+  button.classList.add("active");
+}
+
+function main() {
+  const main = document.createElement("main");
+  main.classList.add("main");
+  main.setAttribute("id", "main");
+  return main;
+}
+
+function footer() {
+  const footer = document.createElement("footer");
+  footer.classList.add("footer");
+
+  const copyright = document.createElement("p");
+  copyright.innerHTML = "Copyright © 2021 Denis Lafontant";
+
+  const githubLink = document.createElement("a");
+  githubLink.href = "https://github.com/icebox827";
+
+  const githubIcon = document.createElement("i");
+  githubIcon.classList.add("fab");
+  githubIcon.classList.add("fa-github");
+
+  githubLink.appendChild(githubIcon);
+  footer.appendChild(copyright);
+  footer.appendChild(githubLink);
+
+  return footer;
+}
+
+function loadPage() {
+  const content = document.getElementById("content");
+
+  content.appendChild(header());
+  content.appendChild(main());
+  content.appendChild(footer());
+
+  activeButton(document.querySelector(".button-nav"));
+  loadHome();
+}
+
+export default loadPage;
+
 
