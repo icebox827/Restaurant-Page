87ed49d3158a1e758d2bde71bcc968353bd246b8
Denis Lafontant <icebox827@gmail.com>
2021-03-24 12:02:07 -0400

    Create Nav.js file

--- src/component/nav.js
+++ src/component/nav.js
@@ -1,12 +1,21 @@
 const Nav = (() => {
   const navBar = () => {
     const content = document.getElementById('content');
-    const navElem = document.createElementById('nav');
+    const nav = document.createElementById('nav');
     const home = document.createElementById('button');
     const menu = document.createElementById('button');
     const contact = document.createElementById('button');
 
-    navElem.class
+    nav.className = 'Nav';
+    home.className = 'home';
+    contact.className = 'contact';
+
+    nav.setAttributes('id', 'navbar');
+
+    document.getElementById('content').appendChild(nav);
+    document.getElementById('navbar').appendChild(home);
+    document.getElementById('navbar').appendChild(menu);
+    document.getElementById('navbar').appendChild(contact);
   };
   return {navBar};
 })();
