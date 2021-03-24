13227494ba74c69dead33ee1a9f00a8a90bfb1fb
Denis Lafontant <icebox827@gmail.com>
2021-03-24 11:55:38 -0400

    install babel-loader

---
+++ src/component/nav.js
@@ -0,0 +1,12 @@
+const Nav = (() => {
+  const navBar = () => {
+    const content = document.getElementById('content');
+    const navElem = document.createElementById('nav');
+    const home = document.createElementById('button');
+    const menu = document.createElementById('button');
+    const contact = document.createElementById('button');
+
+    navElem.class
+  };
+  return {navBar};
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
     ],
   },
 };
