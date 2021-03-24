b4e904ff319b82c91f67703ec6ff2cb1b36694bf
Denis Lafontant <icebox827@gmail.com>
2021-03-24 16:23:26 -0400

    Create home page

--- src/home.js
+++ src/home.js
@@ -2,24 +2,24 @@ function home(){
   const home = document.createElement("div");
   home.classList.add("home");
 
-  const dishImage = document.createElement("img");
-  dishImage.src = "img/dish.jpg";
-  dishImage.alt = "meal";
-
-  home.appendChild(dishImage);
   home.appendChild(motto());
+
   return home;
 }
 
 function motto() {
   const motto = document.createElement("h2")
-  motto.innerHTML = "<h2>More than food</h2> <br> <h3>We are a family</h3>";
+  motto.innerHTML = "More than food <br> We are a family";
+
+  return motto;
 }
 
 function loadHome() {
   const main = document.getElementById("main");
   main.innerHTML = "";
   main.appendChild(home());
+
+  return main
 }
 
 export default loadHome;
